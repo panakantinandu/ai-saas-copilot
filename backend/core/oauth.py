5c71eaf1ae84ac6e698f2b9bb878932d4436b02c
Nandu Panakanti <150353077+panakantinandu@users.noreply.github.com>
2026-06-10 16:51:35 -0500

    Update oauth.py

--- backend/core/oauth.py
+++ backend/core/oauth.py
@@ -12,7 +12,5 @@ oauth.register(
     access_token_url="https://github.com/login/oauth/access_token",
     authorize_url="https://github.com/login/oauth/authorize",
     api_base_url="https://api.github.com/",
-    client_kwargs={
-        "scope": "read:user read:org read:org repo"
-    }
+    client_kwargs={"scope": "read:user read:org public_repo"}
 )
