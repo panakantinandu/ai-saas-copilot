0c1cd39bfc4ed1e698dfa5d7f45b1cbacf5ad306
Nandu Panakanti <150353077+panakantinandu@users.noreply.github.com>
2026-06-11 01:33:50 -0500

    Update database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = (
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
     "postgresql://postgres:Momdad%40123@localhost/saascopilot"
 )
 
@@ -15,3 +16,20 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.orm import declarative_base
+
+# DATABASE_URL = (
+#     "postgresql://postgres:Momdad%40123@localhost/saascopilot"
+# )
+
+# engine = create_engine(DATABASE_URL)
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# Base = declarative_base()
