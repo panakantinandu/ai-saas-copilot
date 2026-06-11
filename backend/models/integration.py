5501ae45b177eddb46751bc3e57af514e9dfd2ef
Nandu Panakanti <150353077+panakantinandu@users.noreply.github.com>
2026-06-11 01:50:34 -0500

    Update integration.py

--- backend/models/integration.py
+++ backend/models/integration.py
@@ -7,6 +7,6 @@ class Integration(Base):
     __tablename__ = "integrations"
 
     id = Column(Integer, primary_key=True)
-    organization_id = Column(Integer, ForeignKey("organizations.id"))
+    organization_id = Column(Integer)
     provider = Column(String)
     status = Column(String)
